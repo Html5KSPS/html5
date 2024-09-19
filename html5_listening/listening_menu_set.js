9bd613f06adcbe355823ea8ec3408eae12589f61
Html5KSPS <html5@ksps.ntct.edu.tw>
2024-09-19 18:01:51 +0800

    Update listening_menu_set.js

--- html5_listening/listening_menu_set.js
+++ html5_listening/listening_menu_set.js
@@ -1,6 +1,4 @@
 ﻿menu_title='聽音辨字六選一';
 menu_items = new Array(
-  '範例1:綜合展示,questions-set-1.js'
-,  '範例2:只放圖片檔名,questions-set-2.js'
-,  'ㄅㄆㄇ四選一,questions-set-3.js'
+ 'ㄅㄆㄇ四選一,questions-set-3.js'
 );
